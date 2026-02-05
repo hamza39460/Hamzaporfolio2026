750013b8f37d7e4f628f8abff04f0745c39a428d
Muhammad Hamza <35405286+hamza39460@users.noreply.github.com>
2026-02-05 14:56:12 +0500

    Fixed Vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/Hamzaporfolio2026/'
+    base: '/Hamzaporfolio2026/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
