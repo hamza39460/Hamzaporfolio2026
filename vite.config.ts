e80e565edb524dd95d857bfc45fb667b47d0c422
Hamza <hamza39460@gmail.com>
2026-02-05 15:42:29 +0500

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/Hamzaporfolio2026/',
+    base: '/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
